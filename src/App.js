e8f8de4fa2dfa1ccefc20d29ed83ceaddc82b57d
Kw <Kwhitlock1217@gmail.com>
2021-04-22 16:51:47 -0700

    add more card styling, fix alignment of columns

--- src/App.js
+++ src/App.js
@@ -25,6 +25,25 @@ function App() {
     fetchPokemon();  
   }, []);
 
+  const nextPokemon = async () => {
+    setLoading(true);
+    let data = await getAll(nextPoke)
+    await loadPokemon(data.results)
+    setNextPoke(data.next)
+    setPrevPoke(data.previous)
+    setLoading(false);
+  }
+
+  const prevPokemon = async () => {
+    if (!prevPoke) return;
+    setLoading(true);
+    let data = await getAll(prevPoke)
+    await loadPokemon(data.results)
+    setNextPoke(data.next)
+    setPrevPoke(data.previous)
+    setLoading(false);
+  }
+
   const loadPokemon = async data => {
     // make a new array from the results of 1st fetch call with the pokemon stats
     let _pokemonData = await Promise.all(
@@ -40,15 +59,30 @@ function App() {
 
   console.log(pokeData);
 
-
-  return (
-  <div className="container-fluid w-50 bg-light rounded my-5">
-    <div id="" className="row flex-wrap mx-auto">
-      {pokeData.map((pokemon, i) => {
-        return <CardComponent  />
-      })}
-    </div>
-  </div>
+//go through each pokemon in pokeData array and render the card component
+  return ( 
+    <div>   
+      {loading ? 
+        <div class="d-flex justify-content-center align-items-center mt-5">
+          <h1>Loading...</h1>
+          <div class="spinner-border ml-4" style={{width: "3rem", height: "3rem"}} role="status" aria-hidden="true">
+          </div>
+        </div> 
+        :(
+      <div className="container-fluid my-5 mx-auto">
+        <div className="d-flex justify-content-center">
+          <button onClick={prevPokemon} id="back" type="button" className="btn btn-primary btn-md rounded mr-3 ">Prev</button>
+          <button onClick={nextPokemon} id="forward" type="button" className="btn btn-primary btn-md rounded mr-3 ">Next</button>
+        </div>  
+        
+        <div id="" className="row justify-content-around">
+          {pokeData.map((pokemon, i) => {
+            return <CardComponent key={i} pokemon={pokemon} />
+          })}
+        </div>
+      </div>
+      )}
+    </div> 
   );
 }
 
--- src/components/card/CardComponent.js
+++ src/components/card/CardComponent.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import './style.css';
 
+
 function CardComponent({pokemon}) {
     return (
-        <div className="card"></div>
+        <div className="card col-3 text-center rounded bg-light m-3 p-3" style={{maxWidth: 300}}>
+            <div className="">
+                <img className="pokemon-img"src={pokemon.sprites.front_default} alt="" />
+            </div>
+            <div className="pokemon-types d-flex justify-content-center">
+                {pokemon.types.map(type => {
+                    return (
+                        <h1 className="pokemon-type badge badge-dark mx-2 p-2">
+                        {type.type.name}
+                        </h1>
+                    );
+                })}
+            </div>
+            <div className="pokemon-info">
+                <p className="mb-0">Height: {pokemon.height} | Weight: {pokemon.weight}</p>
+            </div>
+            <div className="pokemon-name text-capitalize mt-2">
+                <h3>{pokemon.id}. {pokemon.name}</h3>
+            </div>  
+        </div>
     )
 }
 
